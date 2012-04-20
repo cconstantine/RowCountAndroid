e279d312515697c68257f3bd9c5f78efa61ce852
Chris Constantine <chris@heyzap.com>
2012-04-19 22:00:58 -0700

    basic trivial functionality

--- src/com/gamuphi/rowcounts/RowCountsActivity.java
+++ src/com/gamuphi/rowcounts/RowCountsActivity.java
@@ -2,12 +2,33 @@ package com.gamuphi.rowcounts;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RowCountsActivity extends Activity {
+	int count = 0;
+	private EditText CountText;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        CountText = (EditText)this.findViewById(R.id.editText);
+        
+        CountText.setText(String.format("%d",count));
+    }
+    
+    public void onCount(View v) {
+    	count = Integer.parseInt(CountText.getText().toString());
+    	count++;
+        CountText.setText(String.format("%d",count));    	
+    }
+    
+    
+    public void onReset(View v) {
+    	count = 0;
+        CountText.setText(String.format("%d",count)); 
     }
 }
